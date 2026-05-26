3aface4280e91da8cf1c800ae61a4b0256f43784
Sher Mendoza <ingsistemasher03@gmail.com>
2026-05-26 15:26:36 -0500

    Actualizacion

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './database/prisma.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
   imports: [PrismaModule],
--- apps/api/src/modules/products/products.service.ts
+++ apps/api/src/modules/products/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../database/prisma.service';
+import { PrismaService } from '../../database/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
